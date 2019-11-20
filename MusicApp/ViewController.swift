46e2069b23a2250ab6dcb5cc437f06851ef546b6
Abhinav Bhardwaj <abhinavbhardwaj034@gmail.com>
2019-11-20 13:33:00 -0500

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Abhinav Bhardwaj on 2019-11-20.
+//  Copyright © 2019 Abhinav Bhardwaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
